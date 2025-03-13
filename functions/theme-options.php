7f66793ad76dad586080a52b6b77ef1c8fdebd76
Marcos Macedo <marcosmacedo.fogao@gmail.com>
2025-03-13 10:42:57 -0300

    GTM

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -217,3 +217,28 @@ function custom_swiper_gallery_ram3500()
   return ob_get_clean();
 }
 add_shortcode('swiper_gallery_ram3500', 'custom_swiper_gallery_ram3500');
+
+// Adiciona o Google Tag Manager no <head>
+function adicionar_gtm_no_head() {
+  ?>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-TKWWDSCC');</script>
+  <!-- End Google Tag Manager -->
+  <?php
+}
+add_action('wp_head', 'adicionar_gtm_no_head');
+
+// Adiciona o Google Tag Manager (noscript) logo após a tag <body>
+function adicionar_gtm_no_body() {
+  ?>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKWWDSCC"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  <?php
+}
+add_action('wp_body_open', 'adicionar_gtm_no_body');
